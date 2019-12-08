e6a3c775afd990f6009083a2442ad7ec61d37724
peacecop kalmer: <pandeero@gmail.com>
2019-12-08 06:28:10 +0200

    separate files for different categories for not doubling the code

---
+++ merge-sort-chart-doubles-inside.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace mergeSortChart;
+
+/**
+ * Created on 14.09.2015
+ *
+ * @copyright Copyright &copy; 2015, Kalmer Piiskop <pandeero@gmail.com>
+ */
+require_once dirname(__FILE__) . '/MergeSortChart.php';
+
+/**
+ *
+ * @author kalmer
+ * @var string[string][] index is either <code>winner</code> or
+ *      <code>looser</code>, value is the name of the player
+ */
+$matches = array (
+	// HaValÜTeK SeTt 2014
+	/*
+	 * R: Helena - Kalmer / Kalmer - Illimar
+	 * R: Riina - Kaie / Kaie - Helena
+	 * A: Dina / Helena - Riina / Kaie
+	 * T: Dina - Helena / Dina - Illimar
+	 * R: Dina - Illimar / Helena - Kalmer
+	 * R: Helena - Kalmer / Dina - Helena
+	 * A: Oliver - Per / Kalmer - Illimar
+	 */
+	array (
+		'winner' => 'Illimar Sööt - peacecop kalmer:',
+		'looser' => 'kalmer - helena'
+	),
+	array (
+		'winner' => 'riina - kaie',
+		'looser' => 'Kaie - Helena'
+	),
+	array (
+		'winner' => 'Dina - Helena',
+		'looser' => 'riina - kaie'
+	),
+	array (
+		'winner' => 'Dina - Illimar',
+		'looser' => 'Dina - Helena'
+	),
+	array (
+		'winner' => 'Dina - Illimar',
+		'looser' => 'kalmer - helena'
+	),
+	array (
+		'winner' => 'kalmer - helena',
+		'looser' => 'Dina - Helena'
+	),
+	array (
+		'winner' => 'Illimar Sööt - peacecop kalmer:',
+		'looser' => 'Oliver - Per'
+	),
+	// HaValÜTeK SeTt 2015
+	/*
+	 * O: boris - kaie / kalmer - helena
+	 * R: tarmo - pentti / kalmer - illimar
+	 * O: kalmer - helena / illimar - külli
+	 * O: rainer - riina / boris - kaie
+	 * A: rainer - riina / illimar - külli
+	 * O: riina - kaie / helena - külli
+	 * O: georg - boris / kalmer - illimar
+	 * A: kalmer - illimar / rainer - per
+	 * P: pentti - palmsaar / per - rainer
+	 */
+	array (
+		'winner' => 'boris - kaie',
+		'looser' => 'kalmer - helena'
+	),
+	array (
+		'winner' => 'pentti - palmsaar',
+		'looser' => 'Illimar Sööt - peacecop kalmer:'
+	),
+	array (
+		'winner' => 'illimar - külli',
+		'looser' => 'kalmer - helena'
+	),
+	array (
+		'winner' => 'rainer - riina',
+		'looser' => 'boris - kaie'
+	),
+	array (
+		'winner' => 'illimar - külli',
+		'looser' => 'rainer - riina'
+	),
+	array (
+		'winner' => 'helena - külli',
+		'looser' => 'riina - kaie'
+	),
+	array (
+		'winner' => 'Illimar Sööt - peacecop kalmer:',
+		'looser' => 'georg - boris'
+	),
+	array (
+		'winner' => 'per - rainer',
+		'looser' => 'Illimar Sööt - peacecop kalmer:'
+	),
+	array (
+		'winner' => 'pentti - palmsaar',
+		'looser' => 'per - rainer'
+	),
+	// HaValPaTeK 2018
+	/*
+	 * D: eerik - georg kangur / Relika Aruoja Aruoja - Siim Simson
+	 * D: Erik - georg kangur / Kaie Kree - peacecop kalmer:
+	 * D: Erik - georg / Indrek Jakobson - kalmer:
+	 * D: georg kangur - Indrek Jakobson / kalmer
+	 */
+	array (
+			'winner' => 'Illimar Sööt - peacecop kalmer:',
+			'looser' => 'Erik - georg kangur'
+		),
+	array (
+			'winner' => 'Erik - georg kangur',
+			'looser' => 'Relika Aruoja Aruoja - Siim Simson'
+		),
+	array (
+			'winner' => 'Kaie Kree - peacecop kalmer:',
+			'looser' => 'Erik - georg kangur'
+		),
+	array (
+			'winner' => 'Indrek Jakobson / peacecop kalmer:',
+			'looser' => 'Erik - georg kangur'
+		)
+);
+
+$mergeSortChart = new MergeSortChart ();
+$mergeSortChart->manageResults ( array (
+	'matches' => $matches
+) );
+
---
+++ merge-sort-chart-singles-inside.php
@@ -0,0 +1,250 @@
+<?php
+
+namespace mergeSortChart;
+
+/**
+ * Created on 14.09.2015
+ *
+ * @copyright Copyright &copy; 2015, Kalmer Piiskop <pandeero@gmail.com>
+ */
+require_once dirname(__FILE__) . '/MergeSortChart.php';
+
+/**
+ *
+ * @author kalmer
+ * @var string[string][] index is either <code>winner</code> or
+ *      <code>looser</code>, value is the name of the player
+ */
+$matches = array (
+	// HaValÜTeK SeTt 2014
+	/*
+	 * T: Riina / Niels
+	 * R: Dina / Illimar
+	 * R: Marek - Kalmer
+	 * A: Oliver Kotkas / Genadi Nikolajev
+	 * T: Indrek - Rando
+	 * R: Riina / Dina
+	 * A: Rando / Genadi
+	 * T: Indrek / Illimar
+	 * R: Oliver / Kalmer
+	 * T: Genadi / Marek
+	 */
+	array (
+		'winner' => 'Niels',
+		'looser' => 'Riina'
+	),
+	array (
+		'winner' => 'Illimar Sööt',
+		'looser' => 'Dina'
+	),
+	array (
+		'winner' => 'peacecop kalmer:',
+		'looser' => 'Marek'
+	),
+	array (
+		'winner' => 'Oliver Kotkas',
+		'looser' => 'Genadi Nikolajev'
+	),
+	array (
+		'winner' => 'Rando',
+		'looser' => 'Indrek Pajuste'
+	),
+	array (
+		'winner' => 'Dina',
+		'looser' => 'Riina'
+	),
+	array (
+		'winner' => 'Genadi Nikolajev',
+		'looser' => 'Rando'
+	),
+	array (
+		'winner' => 'Indrek Pajuste',
+		'looser' => 'Illimar Sööt'
+	),
+	array (
+		'winner' => 'peacecop kalmer:',
+		'looser' => 'Oliver Kotkas'
+	),
+	array (
+		'winner' => 'Marek',
+		'looser' => 'Genadi Nikolajev'
+	),
+	// HaValÜTeK SeTt 2015
+	/*
+	 * R: kalmer / boris
+	 * R: kalmer / rainer
+	 * P: illimar / külli
+	 * P: kalmer - indrek
+	 * O: indrek / illimar
+	 * D: georg / illimar
+	 * P: tarmo / kalmer
+	 * P: rainer - illimar
+	 * O: indrek / rainer
+	 * O: tarmo / rainer
+	 * O: tarmo / indrek
+	 * D: georg / külli
+	 * O: indrek / georg
+	 * A: kalmer / illimar
+	 * A: kalmer / pentti
+	 * P: boris / pentti
+	 * O: boris / kalmer
+	 * A: rainer / per
+	 * P: tarmo / per
+	 * D: tarmo / georg
+	 */
+	array (
+		'winner' => 'boris',
+		'looser' => 'peacecop kalmer:'
+	),
+	array (
+		'winner' => 'peacecop kalmer:',
+		'looser' => 'rainer'
+	),
+	array (
+		'winner' => 'Illimar Sööt',
+		'looser' => 'külli'
+	),
+	array (
+		'winner' => 'peacecop kalmer:',
+		'looser' => 'Indrek Pajuste'
+	),
+	array (
+		'winner' => 'Indrek Pajuste',
+		'looser' => 'Illimar Sööt'
+	),
+	array (
+		'winner' => 'Illimar Sööt',
+		'looser' => 'georg kangur'
+	),
+	array (
+		'winner' => 'peacecop kalmer:',
+		'looser' => 'tarmo'
+	),
+	array (
+		'winner' => 'rainer',
+		'looser' => 'Illimar Sööt'
+	),
+	array (
+		'winner' => 'Indrek Pajuste',
+		'looser' => 'rainer'
+	),
+	array (
+		'winner' => 'tarmo',
+		'looser' => 'rainer'
+	),
+	array (
+		'winner' => 'tarmo',
+		'looser' => 'Indrek Pajuste'
+	),
+	array (
+		'winner' => 'georg kangur',
+		'looser' => 'külli'
+	),
+	array (
+		'winner' => 'georg kangur',
+		'looser' => 'Indrek Pajuste'
+	),
+	array (
+		'winner' => 'peacecop kalmer:',
+		'looser' => 'Illimar Sööt'
+	),
+	array (
+		'winner' => 'pentti',
+		'looser' => 'peacecop kalmer:'
+	),
+	array (
+		'winner' => 'boris',
+		'looser' => 'pentti'
+	),
+	array (
+		'winner' => 'boris',
+		'looser' => 'peacecop kalmer:'
+	),
+	array (
+		'winner' => 'per',
+		'looser' => 'rainer'
+	),
+	array (
+		'winner' => 'tarmo',
+		'looser' => 'per'
+	),
+	array (
+		'winner' => 'tarmo',
+		'looser' => 'georg kangur'
+	),
+	// HaValÜTeK SeTt 2018
+	/*
+	 * O: peacecop kalmer: / Indrek Pajuste
+	 * O: Gerd Eston Sepp / Siim Simson
+	 * O: Gerd Eston Sepp / Relika Aruoja Aruoja
+	 * P: Illimar Sööt / Indrek Pajuste
+	 * D: eerik / georg kangur
+	 * P: Kaie Kree / Relika Aruoja Aruoja
+	 * P: Illimar Sööt / Indrek Jakobson
+	 * P: Indrek Pajuste / Siim Simson
+	 * P: Illimar Sööt / Siim Simson
+	 * P: Indrek Jakobson / Indrek Pajuste
+	 * P: Illimar Sööt / Indrek Jakobson
+	 * P: Indrek Jakobson / Siim Simson
+	 * D: Erik / Indrek Jakobson
+	 * P: Gerd Eston Sepp / Indrek Pajuste
+	 */
+	array (
+		'winner' => 'peacecop kalmer:',
+		'looser' => 'Erik'
+	),
+	array (
+		'winner' => 'peacecop kalmer:',
+		'looser' => 'Indrek Pajuste'
+	),
+	array (
+		'winner' => 'Siim Simson',
+		'looser' => 'Gerd Eston Sepp'
+	),
+	array (
+		'winner' => 'Gerd Eston Sepp',
+		'looser' => 'Relika Aruoja Aruoja'
+	),
+	array (
+		'winner' => 'Illimar Sööt',
+		'looser' => 'Indrek Pajuste'
+	),
+	array (
+		'winner' => 'Erik',
+		'looser' => 'georg kangur'
+	),
+	array (
+		'winner' => 'Relika Aruoja Aruoja',
+		'looser' => 'Kaie Kree'
+	),
+	array (
+		'winner' => 'Siim Simson',
+		'looser' => 'Indrek Pajuste'
+	),
+	array (
+		'winner' => 'Siim Simson',
+		'looser' => 'Illimar Sööt'
+	),
+	array (
+		'winner' => 'Indrek Jakobson',
+		'looser' => 'Indrek Pajuste'
+	),
+	array (
+		'winner' => 'Illimar Sööt',
+		'looser' => 'Indrek Jakobson'
+	),
+	array (
+		'winner' => 'Indrek Jakobson',
+		'looser' => 'Siim Simson'
+	),
+	array (
+		'winner' => 'Erik',
+		'looser' => 'Indrek Jakobson'
+	),
+);
+
+$mergeSortChart = new MergeSortChart ();
+$mergeSortChart->manageResults ( array (
+	'matches' => $matches
+) );
+
